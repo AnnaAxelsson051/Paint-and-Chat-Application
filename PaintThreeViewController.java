4c5466e7a9092be7d3e3766a49a2566867b0b4da
Anna Axelsson <103879144+AnnaAxelsson051@users.noreply.github.com>
2022-10-31 22:02:10 +0100

    Added Rectangles method

--- PaintThreeViewController.java
+++ PaintThreeViewController.java
@@ -4,43 +4,32 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class PaintThreeViewController {
     @FXML
-    private Canvas canvas;
+    public Canvas canvas;
     @FXML
     private ColorPicker colorpicker;
     @FXML
     private TextField penSize;
     @FXML
     private CheckBox eraser;
+    @FXML
+    private Button button;
 
     //attach listener to the canvas with initialize method
     //will be called automatically by fxmlloader
 
-    /*public void initialize(){
-        GraphicsContext graphics = canvas.getGraphicsContext2D();
-        //get a graphicscontext to draw on
-        canvas.setOnMouseDragged(e ->{
-            double size = Double.parseDouble(penSize.getText());
-            //get brushsize
-            double x = e.getX() - size /2;
-            double y = e.getY() - size /2;
-
-            if(eraser.isSelected()){
-                graphics.clearRect(x,y,size,size);
-        //if eraser is selected erase
-            }else{
-                graphics.setFill(colorpicker.getValue());
-        //otherwise draw with selected color
-                graphics.fillRect(x,y,size,size);
-            }
-        });
-    }*/
     public void initialize(){
+
         GraphicsContext graphics = canvas.getGraphicsContext2D();
         //get a graphicscontext to draw on
         canvas.setOnMouseDragged(e ->{
@@ -68,6 +57,22 @@ public class PaintThreeViewController {
     public void onActionSelectShapes(){
     }
     */
+
+
+
+    /*public void onCanvasSquare(MouseEvent mouseEvent){
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.BLUE);
+        gc.fillRect(mouseEvent.getX(), mouseEvent.getY(), 100, 100);
+
+    }*/
+
+    public void onCanvasSquare(MouseEvent mouseEvent){
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.BLUE);
+        gc.fillRect(mouseEvent.getX(), mouseEvent.getY(), 100, 100);
+
+    }
 
     public void onActionExit(){
         Platform.exit();
