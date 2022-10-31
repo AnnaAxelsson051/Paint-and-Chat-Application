a347229a854be537d3a6c1bb00470bfe4c744ba4
Anna Axelsson <103879144+AnnaAxelsson051@users.noreply.github.com>
2022-10-31 20:26:11 +0100

    Initial commit

---
+++ PaintThreeViewController.java
@@ -0,0 +1,57 @@
+package se.iths.tt.javafxtt.controller;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.TextField;
+
+public class PaintThreeViewController {
+    @FXML
+    private Canvas canvas;
+    @FXML
+    private ColorPicker colorpicker;
+    @FXML
+    private TextField brushSize;
+    @FXML
+    private CheckBox eraser;
+
+    //attach listener to the canvas with initialize method
+    //will be called automatically by fxmlloader
+
+    public void initialize(){
+        GraphicsContext graphics = canvas.getGraphicsContext2D();
+        //get a graphicscontext to draw on
+        canvas.setOnMouseDragged(e ->{
+            double size = Double.parseDouble(brushSize.getText());
+            //get brushsize
+            double x = e.getX() - size /2;
+            double y = e.getY() - size /2;
+
+            if(eraser.isSelected()){
+                graphics.clearRect(x,y,size,size);
+        //if eraser is selected erase
+            }else{
+                graphics.setFill(colorpicker.getValue());
+        //otherwise draw with selected color
+                graphics.fillRect(x,y,size,size);
+            }
+        });
+    }
+
+
+    /*public void onActionSave(){
+
+    }
+
+    public void onActionSelectShapes(){
+    }
+    */
+
+    public void onActionExit(){
+        Platform.exit();
+
+    }
+}
